5e588dc69bf775f189ff10a29b28b88fda597390
Chuvak <slvno@rambler.ru>
2023-02-14 00:45:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-python -m venv
+#apt install python-is-python3 python3.10-venv
+python -m venv ./
 source ./bin/activate
-pip freeze > requirements.list
+#pip freeze > requirements.list
 pip install -r requirements.list
